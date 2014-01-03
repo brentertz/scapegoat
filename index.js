ebb2706aed1565b9411c4935735398ca2c0e97d3
Brent Ertz <brent.ertz@gmail.com>
2014-01-03 13:49:11 -0700

    Implement escape and unescape functions

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * Scapegoat
+ * https://github.com/brentertz/scapegoat
+ *
+ * Copyright (c) 2014 Brent Ertz
+ * Licensed under the MIT license.
+ */
+
+/**
+ * Escape special characters in the given string of html.
+ *
+ * @param  {String} html
+ * @return {String}
+ */
+module.exports = {
+  escape: function(html) {
+    return String(html)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  },
+
+  /**
+   * Unescape special characters in the given string of html.
+   *
+   * @param  {String} html
+   * @return {String}
+   */
+  unescape: function(html) {
+    return String(html)
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&#39;/g, '\'')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>');
+  }
+};
